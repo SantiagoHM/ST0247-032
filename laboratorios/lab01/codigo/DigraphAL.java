3c6787d2698ff8796fb6d4bf0077131cbf8f2bea
aalmanzarr <35697592+aalmanzarr@users.noreply.github.com>
2018-08-04 20:04:03 -0500

    Add files via upload

---
+++ laboratorios/lab01/codigo/DigraphAL.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+import javafx.util.Pair;
+//codigo tomado de fjaram18
+public class DigraphAL extends Digraph {
+    ArrayList<ArrayList<Pair<Integer, Integer>>> vertices;
+    
+    public DigraphAL(int size) {
+        super(size);
+        vertices = new ArrayList<ArrayList<Pair<Integer, Integer>>>(size);
+        
+        for (int i = 0; i < size; i++) {
+            ArrayList<Pair<Integer, Integer>> verVacio = new ArrayList<Pair<Integer, Integer>>();
+            vertices.add(verVacio);
+        }
+    }
+
+    public void addArc(int source, int destination, int weight) {
+        vertices.get(source).add( new Pair<>(destination, weight) );
+    }
+
+    public ArrayList<Integer> getSuccessors(int vertex) {
+        ArrayList<Pair<Integer, Integer>> vertx = vertices.get(vertex);
+        ArrayList<Integer> solucion = new ArrayList<Integer>();
+        
+        for (int i=0; i<vertx.size(); i++) {
+            int vecino = vertx.get(i).getKey();
+            solucion.add(vecino);
+        }
+        
+        
+        return solucion;
+    }
+
+    public int getWeight(int source, int destination) {
+        ArrayList<Pair<Integer, Integer>> vertx  = vertices.get(source);
+        
+        for (int i=0; i<vertx.size(); i++){
+            int adj = vertx.get(i).getKey();
+            if (adj == destination) {
+                return vertx.get(i).getValue();
+            }
+        }
+        return 0;
+    }
+
+}
---
+++ laboratorios/lab01/codigo/GraphAlgorithms.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+//codigo tomado de fjaram18
+public class GraphAlgorithms
+{
+    public static int verticePopular(Digraph digraph) {
+        int size = digraph.size();
+        int ans = 0;
+        for (int i = 0; i < size; i++) {
+            ArrayList <Integer> lista = digraph.getSuccessors(i);
+            if (ans < lista.size()) {
+                ans = lista.size();
+            }
+        }
+        return ans;
+    }
+}
