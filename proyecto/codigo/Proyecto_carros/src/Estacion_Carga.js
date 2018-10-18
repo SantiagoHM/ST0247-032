ab3dd7473cc9ec03840749c09db771dce17bb260
Isaias Labrador <ilabradors@eafit.edu.co>
2018-10-17 23:12:55 -0500

    "añadiendo cliente y NODO y corrigiendo herencia en estacion de carga"

--- proyecto/codigo/Proyecto_carros/src/Estacion_Carga.js
+++ proyecto/codigo/Proyecto_carros/src/Estacion_Carga.js
@@ -1,6 +1,8 @@
 "use strict";
 
-function Estacion_Carga(tipoEstacion, relacionCarga){
+function Estacion_Carga(posMA, posXY, id, tipoEstacion, relacionCarga){
+    Nodo.call(this, posMA, posXY, id);
+
     this.tipoEstacion = tipoEstacion;
     this.relacionCarga = relacionCarga;
 }
---
+++ proyecto/codigo/Proyecto_carros/src/Nodo.js
@@ -0,0 +1,7 @@
+"use strict";
+
+function Nodo(posMA, posXY, id){
+  this.posMA = posMA;
+  this.posXY = posXY;
+  this.id = id;
+}
