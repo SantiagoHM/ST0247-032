9d985783130db525e1e5987b6731a01aeb982070
Isaias Labrador <ilabradors@eafit.edu.co>
2018-10-31 21:45:18 -0500

    Avances

--- proyecto/codigo/Proyecto_carros/src/Algoritmo.js
+++ proyecto/codigo/Proyecto_carros/src/Algoritmo.js
@@ -13,12 +13,12 @@ var nodos = new Array();
 var clientes= new Array();
 var visited;
 var estaciones = new Array();
-var g;
+var grafo;
 var ruta; //Recorrido total que se ha realizado
 
 //Clase del algoritmo principal.
 function algoritmo(){
-  this.name = "hola";
+
 
   this.putInfo = function(text){
     N = text[0];
@@ -32,9 +32,16 @@ function algoritmo(){
     st_customer = text[8];
     Q = text[9];
 
+    //grafo = new DigraphAM(N);
+
+    var times = new Array();
+    var wat = new Array();
+    var info = new Array();
+
+
     for(var i = 11 ; i< text.length; ++i){
       if(text[i] != ""){
-        var info = new Array();
+
         info = text[i].split(" ");
         var id = parseInt(info[0])
         var aux = new Nodo(id, info[1], parseFloat(info[2]), parseFloat(info[3]), info[4], parseInt(info[5]), 0);
@@ -52,8 +59,48 @@ function algoritmo(){
         break;
       }
     }
-    console.log(nodos);
-    console.log(estaciones);
-    console.log(clientes);
+
+
+/**
+    for (int j = 0; j < g.matriz.length; j++) {
+      for (int k = 0; k < g.matriz[j].length; k++) {
+        //System.out.println(nodes[j].x+" "+ nodes[j].y+" "+ nodes[k].x+" "+ nodes[k].y);
+        grafo.matriz[j][k] = calcularDistancia(nodes[j].x, nodes[j].y, nodes[k].x, nodes[k].y);
+      }
+    }
+*/
+
+  info = text[17].split(" ");
+  times.push(parseFloat(info[3]));
+  info = text[18].split(" ");
+  times.push(parseFloat(info[3]));
+  info = text[19].split(" ");
+  times.push(parseFloat(info[3]));
+
+  console.log(times);
+
+  info = text[21].split(" ");
+  wat.push(parseFloat(info[3]));
+  info = text[22].split(" ");
+  wat.push(parseFloat(info[3]));
+  info = text[23].split(" ");
+  wat.push(parseFloat(info[3]));
+
+  console.log(wat);
+
+  var tipos = new Array();
+
+  for(var aux = 0; aux < 3; i++){
+    tipos.push(wat[aux]/times[aux]);
   }
+
+  console.log(tipos);
+
+  }
+
+
+}
+
+function calcularDistancia(x1, y1, x2, y2){
+  return Math.sqrt(Math.pow(x1 + y1, 2) + Math.pow(x2+y2, 2));
 }
--- proyecto/codigo/Proyecto_carros/src/DigraphAM.js
+++ proyecto/codigo/Proyecto_carros/src/DigraphAM.js
@@ -7,36 +7,32 @@
   /*Se crea y se inicializa la matriz; la  matriz es cuadrada
   su tamaño viene dado
   por el numero de vertices.*/
-  function DigraghAm(size){
-    this.size=size
-   matriz =new Array(size);
-    for(var i=0;i<matriz.length;i++){
-      arr[i]=new Array(size);
-    }
-    return matriz;
+  function constructor(size){
+   this.size=size
+   this.matriz =new Array(size);
+      for(var i=0;i<matriz.length;i++){
+        this.matriz[i]=new Array(size);
+      }
   }
 
   }
   //Metodo addArc. Este metodo añade un arco al grafo en una matriz.
   function addArc(source,destination,weight){
-    matriz[source][destination]=weight;
+    this.matriz[source][destination]=weight;
   }
 
   //Metodo getWeight. Este metodo retorna el peso de un arco en el grafo
    function getWeight(source,destination){
-     return matriz[source][destination];
+     return this.matriz[source][destination];
    }
    /*Metodo getSuccessors. Este metodo retorna un ArrayList con los sucesores
    de un nodo dado en el grafo.*/
-  ArrayList<integer> getSucceessors(vertices){
-    ArrayList<Integer> array = new ArrayList<>();
-    for (var i = 0; i < matriz[vertex].length; i++) {
-           if (matriz[vertex][i] != 0) {
+  function getSucceessors(vertice){
+    var array = new Array();
+    for (var i = 0; i < matriz[vertice].length; i++) {
+           if (matriz[vertice][i] > 0) {
                array.push(i);
            }
-       }
-       if (array.isEmpty()) {
-           return null;
-       }
-       return array;
+    }
+     return array;
   }
